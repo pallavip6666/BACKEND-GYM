7dc10b28b46f806019b4585cc9809fd765eaf8a2
pallavip6666 <89444247+pallavip6666@users.noreply.github.com>
2024-12-13 20:15:50 +0530

    yes

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,19 @@ config({ path: "./.env" });
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL?.split(",") || ["http://localhost:3000"],
-    methods: ["POST"],
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL?.split(",") || ["http://localhost:3000"],
+//     methods: ["POST"],
+//     credentials: true,
+//   })
+// );
 
 // app.use(
 //   cors({
